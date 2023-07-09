d90f276fbffa90eb0beae4dbc51d077bff5ac52c
TYTOY46 <gvhg0503@gmail.com>
2023-07-09 15:51:28 +0300

    Исправление неполадок

---
+++ C2/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.http import  HttpResponse
+from .models import Worker
+
+def qw(request):
+    return render(request , '12im.html')
+#,{'h1':h1}
+def qw1(request):
+    return render (request , '13im.html')
+def donat (request):
+    return render(request,'14.html')
