9ba66c73d6a4bd9014ef985ac01d721a80ab8578
TYTOY46 <gvhg0503@gmail.com>
2023-07-25 14:38:55 +0300

    Меняет названия html файлов и удаляет папку статик (я гуглил не чо не нашол)

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('' , views.qw1, name='qw1'),
     path('donat' , views.donat,name='donat'),
-
-#    path('qw', views.qw, name='qw'),
+    path('qw', views.qw, name='qw'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -4,10 +4,10 @@ from django.http import  HttpResponse
 from .models import Worker
 
 def qw(request):
-    return render(request , '12im.html')
+    return render(request , 'files_html\donat.html')
 def qw1(request):
-    return render (request , '13im.html')
+    return render (request , 'files_html\main_page.html')
 def donat (request):
-    return render(request,'14.html')
+    return render(request,'files_html\workers.html')
 
 
